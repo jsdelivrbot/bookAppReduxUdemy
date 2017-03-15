5bf69002290e06c75eba6b3e592e6fcce36b4636
antong <antong@netvertise.com>
2017-03-15 20:26:45 +0200

    rendering the booklist

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -1,9 +1,10 @@
 import React, {Component} from 'react';
+import { connect } from 'react-redux';
 
-export default class BookList extends Component {
+
+class BookList extends Component {
   constructor(props){
     super(props);
-
   }
 
   renderList() {
@@ -24,3 +25,12 @@ export default class BookList extends Component {
     )
   }
 }
+
+
+function mapStateToProps(state) {
+  return {
+    books: state.books
+  }
+}
+
+export default connect(mapStateToProps)(BookList)
