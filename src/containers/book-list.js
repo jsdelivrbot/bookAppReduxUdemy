63900b3c10fbb47c46d46efb96a9331cb1fe1afa
antong <antong@netvertise.com>
2017-03-15 21:00:37 +0200

    adding the action and Dispatch

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import { connect } from 'react-redux';
-
+import {selectBook} from '../actions'
+import {bindActionCreators} from 'redux'
 
 class BookList extends Component {
   constructor(props){
@@ -11,6 +12,7 @@ class BookList extends Component {
     return this.props.books.map((book, index) => (
       <li
         key={index}
+        onClick={() => this.props.selectBook(book)}
         className='list-group-item'>
         {book.title}
       </li>
@@ -33,4 +35,8 @@ function mapStateToProps(state) {
   }
 }
 
-export default connect(mapStateToProps)(BookList)
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({selectBook: selectBook}, dispatch)
+}
+
+export default connect(mapStateToProps,mapDispatchToProps)(BookList)
